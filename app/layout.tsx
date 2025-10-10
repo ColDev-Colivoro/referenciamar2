28364d67ee19569c421c290517b00b84676dcbde
Argonozo <elmecaniqueador@gmail.com>
2025-10-10 15:39:57 -0300

    tittle

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'ColDev',
+  description: 'Creado por ColDev Desarrollador',
+  generator: 'VERSION 1',
 }
 
 export default function RootLayout({
